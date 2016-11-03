637aecb4f5e2045a2a4516322f6ffb4ebb7d7271
Tegan Duong <teganduong@gmail.com>
2016-11-03 14:36:50 -0700

    Write test for Users.getUserById

--- test/Helpers.js
+++ test/Helpers.js
@@ -7,7 +7,7 @@ const Helpers = {
   ip_address: '127.0.0.1',
   fingerprint: process.env.FINGERPRINT,
   // fill these in with values associated with your own keys
-  user_id: '57d2055a86c27339ffdee4cc',
+  user_id: '57f9a1a786c27334da6a48b6',
   node_id: '57ec57be86c27345b3f8a159',
   to_node_id: '57f4241d86c27331523e2f26',
   trans_id: '57fc1a6886c2732e64a94c25'
