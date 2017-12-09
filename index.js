88b8ca353f910b6217b11089ac31330a297a2e5a
tynahuynh <tynahuynh@gmail.com>
2017-12-08 20:58:05 -0800

    fixed merge conflicts

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ var Clients = require('./lib/Clients.js'),
     User = require('./lib/User.js'),
     Nodes = require('./lib/Nodes.js'),
     Node = require('./lib/Node.js'),
-<<<<<<< HEAD
     Subnets = require('./lib/Subnets.js'),
     Subnet = require('./lib/Subnet.js'),
     Subscriptions = require('./lib/Subscriptions.js'),
     Subscription = require('./lib/Subscription.js'),
-=======
->>>>>>> 387f5b1a7b3a233ecc39669c27a7e1507991ccd9
     Transactions = require('./lib/Transactions.js'),
     Transaction = require('./lib/Transaction.js'),
     Helpers = require('./lib/Helpers.js');
@@ -20,13 +17,10 @@ module.exports = {
   User: User,
   Nodes: Nodes,
   Node: Node,
-<<<<<<< HEAD
   Subnets : Subnets,
   Sunbet : Subnet,
   Subscriptions : Subscriptions,
   Subscription : Subscription,
-=======
->>>>>>> 387f5b1a7b3a233ecc39669c27a7e1507991ccd9
   Transactions: Transactions,
   Transaction: Transaction,
   Helpers: Helpers
--- test/Helpers.js
+++ test/Helpers.js
@@ -1,26 +1,10 @@
 'use strict';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 387f5b1a7b3a233ecc39669c27a7e1507991ccd9
 var Clients = require('../lib/Clients.js');
 
 const Helpers = {
   client: new Clients(process.env.CLIENT_ID, process.env.CLIENT_SECRET, false),
   ip_address: '127.0.0.1',
-<<<<<<< HEAD
-  fingerprint: 'c7e516361e6e5453de96223616d73a7b',
-  // fill these in with values associated with your own keys
-  user_id: '5a29b636baabfc00319bea27',
-  // Make sure node_id provided is allowed: "CREDIT-AND-DEBIT" permissions
-  node_id: '5a29cbc0f87333003ac3faf8',
-  // Make sure to_node_id provided has type: 'SYNAPSE-US' 
-  to_node_id: '5a2771782676e7003a574eca',
-  trans_id: '5a29d9b4c9ab5b00371241a4',
-  subscription_id: '5a2727481d569f51aa32eaf0',
-  subnets_id: '5a29d6b6dcfde000221dbf4e'
-=======
   fingerprint: 'fingerprint',
   // fill these in with values associated with your own keys
   user_id: 'userID',
@@ -29,7 +13,7 @@ const Helpers = {
   // Make sure to_node_id provided has type: 'SYNAPSE-US' 
   to_node_id: 'toNodeID',
   trans_id: 'transID'
->>>>>>> 387f5b1a7b3a233ecc39669c27a7e1507991ccd9
+
 };
 
 module.exports = Helpers;
